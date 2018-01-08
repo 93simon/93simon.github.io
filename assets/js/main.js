78e3e9d9d6e5dd248019f8364603947baa403b56
93simon <simon@thedigitalkid.com>
2018-01-08 00:49:54 -0800

    work3

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,7 +131,7 @@
 
 		// Scrolly.
 			$('.scrolly').scrolly({
-				speed: 1000,
+				speed: 800,
 				offset: function() {
 
 					// If <=large, >small, and sidebar is present, use its height as the offset.
